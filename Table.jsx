4dde9ad3f0e27e7ff1163f0f04e177a39b050987
Rifal Nurjamil <81290043+nawcodes@users.noreply.github.com>
2023-10-12 20:10:55 +0700

    Feat: pagination

--- Table.jsx
+++ Table.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Pagination from "./Pagination";
 import Button from "./Button";
 
@@ -82,14 +82,45 @@ const TableData = ({ columns, rowData }) => {
  * @returns
  */
 
-const Table = ({ columns, rowData, actions, onSearch }) => {
-    const handleSearchChange = (event) => {
-        onSearch(event.target.value);
+const Table = ({ columns, rowData, actions, onSearch, itemsPerPage = 5 }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchText, setSearchText] = useState("");
+
+    const searchData = (e) => {
+        setSearchText(e.target.value);
+        setCurrentPage(1);
     };
+
+    // Menghitung indeks data yang akan ditampilkan berdasarkan halaman saat ini
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const filteredData = rowData.filter((item) => {
+        let matchFound = false;
+
+        columns.forEach((column) => {
+            if (column.key !== "actions") {
+                const cellData = column.key.includes(".")
+                    ? column.key
+                          .split(".")
+                          .reduce((acc, curr) => acc && acc[curr], item)
+                    : item[column.key];
+
+                if (cellData) {
+                    const cellDataString = String(cellData).toLowerCase();
+                    if (cellDataString.includes(searchText.toLowerCase())) {
+                        matchFound = true;
+                    }
+                }
+            }
+        });
+
+        return matchFound;
+    });
 
     const columnsWithNumbering = [{ header: "#", key: "_number" }, ...columns];
 
-    const dataWithNumbering = rowData.map((value, index) => {
+    const dataWithNumbering = filteredData.map((value, index) => {
         let modifiedActions = actions;
 
         if (modifiedActions) {
@@ -108,7 +139,7 @@ const Table = ({ columns, rowData, actions, onSearch }) => {
                                       /{([^}]+)}/g,
                                       (match, captureGroup) => {
                                           // Found the data
-                                          
+
                                           let payload = captureGroup
                                               .split(".")
                                               .reduce(
@@ -143,37 +174,36 @@ const Table = ({ columns, rowData, actions, onSearch }) => {
                               );
                           }
 
-                        //   an action has a event handler
+                          //   an action has a event handler
                           if (
                               child.props.dataValue !== undefined &&
                               child.props.onClick !== undefined
                           ) {
-                        
-                            let result = ""
-                            //   is data value contain (.)
-                            if(child.props.dataValue.includes('.')) {
-                                let keys = child.props.dataValue.split('.');
-                                result =  keys[0];
-                                for (let inIndex = 1; inIndex < keys.length; inIndex++) {
-                                    result += value + '[' + keys[inIndex] + ']'; 
-                                }
-                            }else {
-                                
-                                result = value[child.props.dataValue];
+                              let result = "";
+                              //   is data value contain (.)
+                              if (child.props.dataValue.includes(".")) {
+                                  let keys = child.props.dataValue.split(".");
+                                  result = keys[0];
+                                  for (
+                                      let inIndex = 1;
+                                      inIndex < keys.length;
+                                      inIndex++
+                                  ) {
+                                      result +=
+                                          value + "[" + keys[inIndex] + "]";
+                                  }
+                              } else {
+                                  result = value[child.props.dataValue];
+                              }
 
-                            }
+                              const { dataValue, ...newAttribute } =
+                                  child.props;
 
-                            const { dataValue, ...newAttribute } =
-                            child.props;
-
-                            return (
-                                <Button
-                                    dataValue={result}
-                                    {...newAttribute}
-                                >
-                                    {child.props.children}
-                                </Button>
-                            )
+                              return (
+                                  <Button dataValue={result} {...newAttribute}>
+                                      {child.props.children}
+                                  </Button>
+                              );
                           }
 
                           return child;
@@ -255,39 +285,37 @@ const Table = ({ columns, rowData, actions, onSearch }) => {
     return (
         <>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                {onSearch && (
-                    <div className="flex items-center justify-between p-4 bg-white dark:bg-gray-900">
-                        <label for="table-search" className="sr-only">
-                            Search
-                        </label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg
-                                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                                    />
-                                </svg>
-                            </div>
-                            <input
-                                type="text"
-                                id="table-search-users"
-                                className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Search for users"
-                                onChange={(e) => handleSearchChange(e)}
-                            />
+                <div className="flex items-center justify-between p-4 bg-white dark:bg-gray-900">
+                    <label for="table-search" className="sr-only">
+                        Search
+                    </label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg
+                                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                />
+                            </svg>
                         </div>
+                        <input
+                            type="text"
+                            id="table-search-users"
+                            className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Search for users"
+                            onChange={(e) => searchData(e)}
+                        />
                     </div>
-                )}
+                </div>
 
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -304,18 +332,25 @@ const Table = ({ columns, rowData, actions, onSearch }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {dataWithNumbering.map((rowData, index) => (
+                    {dataWithNumbering
+                        .slice(startIndex, endIndex) // Menampilkan data sesuai halaman saat ini
+                        .map((filteredData, index) => (
                             <TableData
                                 key={index}
                                 columns={columnsWithNumbering}
-                                rowData={rowData}
+                                rowData={filteredData}
                                 actions={actions}
                             ></TableData>
                         ))}
                     </tbody>
                 </table>
                 <div className="flex justify-end py-4 px-2">
-                    <Pagination></Pagination>
+                    <Pagination
+                        currentPage={currentPage}
+                        totalItems={filteredData.length}
+                        itemsPerPage={itemsPerPage}
+                        onPageChange={(page) => setCurrentPage(page)}
+                    ></Pagination>
                 </div>
             </div>
         </>
