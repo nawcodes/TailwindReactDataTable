fc559c731500013857c83df408ce9b0c3a9142b1
Rifal Nurjamil <81290043+nawcodes@users.noreply.github.com>
2023-10-12 20:11:25 +0700

    Feat: fix pagination

--- Pagination.jsx
+++ Pagination.jsx
@@ -1,4 +1,17 @@
-const Pagination = () => {
+const Pagination = ({
+    currentPage,
+    totalItems,
+    itemsPerPage,
+    onPageChange,
+}) => {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const handlePageChange = (page) => {
+        // Memastikan halaman yang diminta berada dalam rentang yang valid
+        if (page >= 1 && page <= totalPages) {
+            onPageChange(page);
+        }
+    };
+
     return (
         <>
             <nav aria-label="Page navigation example">
@@ -6,11 +19,12 @@ const Pagination = () => {
                     <li>
                         <a
                             href="#"
-                            class="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                            className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                            onClick={() => handlePageChange(currentPage - 1)}
                         >
-                            <span class="sr-only">Previous</span>
+                            <span className="sr-only">Previous</span>
                             <svg
-                                class="w-2.5 h-2.5"
+                                className="w-2.5 h-2.5"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -26,55 +40,30 @@ const Pagination = () => {
                             </svg>
                         </a>
                     </li>
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <li key={index}>
+                            <a
+                                href="#"
+                                className={`flex items-center justify-center px-3 h-8 leading-tight ${
+                                    currentPage === index + 1
+                                        ? "text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                                        : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                }`}
+                                onClick={() => handlePageChange(index + 1)}
+                            >
+                                {index + 1}
+                            </a>
+                        </li>
+                    ))}
                     <li>
                         <a
                             href="#"
-                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover-bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                            onClick={() => handlePageChange(currentPage + 1)}
                         >
-                            1
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            2
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            aria-current="page"
-                            class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                        >
-                            3
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            4
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            5
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            <span class="sr-only">Next</span>
+                            <span className="sr-only">Next</span>
                             <svg
-                                class="w-2.5 h-2.5"
+                                className="w-2.5 h-2.5"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
